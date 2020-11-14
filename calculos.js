d8957f54601066f1ef8352a2cd7003b3d76cdb72
Jose <jose_esteban_basualdo@hotmail.com>
2020-11-13 23:00:34 -0300

    Agrega calculo promedio de edad

--- calculos.js
+++ calculos.js
@@ -57,4 +57,36 @@ function calcularMenorEdad (edades){
   
 
   
+function calcularPromedioEdad(edades){
+
+    let $valoresEdad = document.querySelectorAll('.nuevo-integrante');
+  
+    let $promedioEdad = document.querySelector('#promedio');
+  
+    let sumaEdades = 0;
+  
+    let promedioEdades = 0;
+  
+  
+    for (i = 0 ; i <$valoresEdad.length ; i++){
+  
+      if($valoresEdad.length > 0){
+  
+      sumaEdades = sumaEdades + Number( $valoresEdad[i].value)
+  
+      
+      }
+      
+   
+  
+      
+   //
+    }
+  
+    promedioEdades = sumaEdades/ $valoresEdad.length;
+  
+    return $promedioEdad.textContent = 'El promedio de edad del grupo familiar es ' + promedioEdades;
+  } 
+
+  
   
